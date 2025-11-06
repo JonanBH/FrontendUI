f1938dc80e07950d15c2db75a103476a5a74ad69
JonanKoey <jonanbh@gmail.com>
2025-11-06 15:21:46 -0300

    feat: (Class 10) Implemented base tags

---
+++ Source/FrontendUI/Public/AbilitySystem/FETags.cpp
@@ -0,0 +1,12 @@
+﻿#include "FETags.h"
+
+namespace FETags
+{
+	namespace UI
+	{
+		UE_DEFINE_GAMEPLAY_TAG(Frontend_WidgetStack_Modal, "FETags.UI.Modal");
+		UE_DEFINE_GAMEPLAY_TAG(Frontend_WidgetStack_GameMenu, "FETags.UI.GameMenu");
+		UE_DEFINE_GAMEPLAY_TAG(Frontend_WidgetStack_GameHud, "FETags.UI.GameHud");
+		UE_DEFINE_GAMEPLAY_TAG(Frontend_WidgetStack_Frontend, "FETags.UI.Frontend");
+	}
+}
---
+++ Source/FrontendUI/Public/AbilitySystem/FETags.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace FETags
+{
+	namespace UI
+	{
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Modal);
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameMenu);
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameHud);
+		UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Frontend);
+	}
+}
